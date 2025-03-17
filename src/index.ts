cf245bc199064ca762f6855da14c908f2cb7b4de
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-03-17 15:13:50 -0300

    Added /personagem and /jogo routes to the app.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotEnv from "dotenv";
+import personagemRoutes from "./routes/personagemRoutes";
+import jogoRoutes from "./routes/jogoRoutes";
 
 dotEnv.config();
 
@@ -8,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/personagem", personagemRoutes);
+app.use("/jogo", jogoRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
